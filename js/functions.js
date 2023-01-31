7d7faae19e75bf6cbc224d0db05f76f16a7f0ab9
rodrigovdv01 <rodrigo.vasquezdevel@utec.edu.pe>
2023-01-31 12:04:11 -0500

    actualize

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,5 @@ let wa = document.getElementById('wa')
 wa.addEventListener("click", abrirWhatsapp)
 
 function abrirWhatsapp() {
-    window.open("https://api.whatsapp.com/send?phone=51961257878&text=Hola! ¿Cuál es el menú de hoy?")
+    window.open("https://api.whatsapp.com/send?phone=51913687390")
 }
