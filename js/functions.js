5e52824db449324d1aa286266d7165cdb87828e2
rodrigovdv01 <rodrigo.vasquezdevel@utec.edu.pe>
2023-02-01 08:44:18 -0500

    type

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,22 @@
-let wa = document.getElementById('wa')
+let ig = document.getElementsByClassName('ig');
+for (let i = 0; i < ig.length; i++) {
+    ig[i].addEventListener("click", abrirIg);
+}
 
-wa.addEventListener("click", abrirWhatsapp)
+let wa = document.getElementsByClassName('wa');
+for (let i = 0; i < wa.length; i++) {
+    wa[i].addEventListener("click", abrirWhatsapp);
+}
+
+let comprar = document.getElementsByClassName('comprar');
+for (let i = 0; i < comprar.length; i++) {
+    comprar[i].addEventListener("click", abrirWhatsapp);
+}
 
 function abrirWhatsapp() {
-    window.open("https://api.whatsapp.com/send?phone=51913687390")
+    window.open("https://api.whatsapp.com/send?phone=51913687390");
+}
+
+function abrirIg() {
+    window.open("https://www.instagram.com/webo.pe/?hl=es-la");
 }
