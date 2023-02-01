c4a436939c0fa704aa8491aa24b483a3d443cc5d
rodrigovdv01 <rodrigo.vasquezdevel@utec.edu.pe>
2023-02-01 15:13:09 -0500

    slider

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,63 @@ function abrirWhatsapp() {
 function abrirIg() {
     window.open("https://www.instagram.com/webo.pe/?hl=es-la");
 }
+
+// Slider
+
+const slider = document.querySelector('.slider');
+const products = document.querySelectorAll('.product');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let current = 0;
+let isEnabled = true;
+
+
+function changeCurrent(newCurrent) {
+    current = newCurrent;
+    slider.style.transform = `translateX(-${current * 25}%)`;
+
+    if (current === 0) {
+        prevBtn.disabled = true;
+        prevBtn.classList.add("disabled");
+    } else {
+        prevBtn.disabled = false;
+        prevBtn.classList.remove("disabled");
+    }
+
+    if (current === products.length - 4) {
+        nextBtn.disabled = true;
+        nextBtn.classList.add("disabled");
+    } else {
+        nextBtn.disabled = false;
+        nextBtn.classList.remove("disabled");
+    }
+}
+
+prevBtn.addEventListener("click", () => {
+    if (isEnabled) {
+        isEnabled = false;
+        if (current === 0) {
+            changeCurrent(products.length - 1);
+        } else {
+            changeCurrent(current - 1);
+        }
+        setTimeout(() => {
+            isEnabled = true;
+        }, 500);
+    }
+});
+
+nextBtn.addEventListener("click", () => {
+    if (isEnabled) {
+        isEnabled = false;
+        if (current === products.length - 1) {
+            changeCurrent(0);
+        } else {
+            changeCurrent(current + 1);
+        }
+        setTimeout(() => {
+            isEnabled = true;
+        }, 500);
+    }
+});
